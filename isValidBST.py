72cee3127ca05558fd0017eaecaeaec38fdb7275
Jiali Chen <scarlett.chen50@gmail.com>
2015-05-24 13:17:26 -0400

    Update isValidBST.py

--- isValidBST.py
+++ isValidBST.py
@@ -13,6 +13,7 @@ class Solution:
     # Scarlett Chen
     # leetcode 98 Validate Binary Search Tree
     # 5/24/2015
+    # 其他方法，走一遍中序遍历，存入数组；确保左边总是比右边小
     def isValidBST(self, root):
         return self.control(root, -sys.maxint, sys.maxint)
       
