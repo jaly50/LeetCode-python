cba7e2721d43c56eac5b819bb126dc6f129f8d44
Jiali Chen <scarlett.chen50@gmail.com>
2015-05-23 21:51:15 -0400

    Update insertionSortList.py

--- insertionSortList.py
+++ insertionSortList.py
@@ -13,14 +13,11 @@ class Solution:
  def insertionSortList(self, head):
         pre =dummy = ListNode(-1)
         cur = head
-        # 有一组数据已排好序，按这个方法会超时。所以在这里判断一下。 java版本不会超时。
-        while cur!=None and cur.next!=None and cur.val < cur.next.val:
-            cur = cur.next
-        if cur==None or cur.next==None:
-            return head
-        cur = head
+        # 有一组数据已排好序，按这个方法会超时。所以在这里判断一下。
         while cur!=None:
-            pre = dummy
+            # optimization for sorted list
+            if pre.val >=cur.val:
+                pre = dummy
             while pre.next !=None and pre.next.val < cur.val:
                 pre = pre.next
             temp = cur.next
