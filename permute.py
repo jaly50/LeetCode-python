3aba8f2fb47cfaef04d4fa7ccf2695a0d07313fb
Jiali Chen <scarlett.chen50@gmail.com>
2015-05-26 22:14:44 -0400

    Create permute.py

---
+++ permute.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    # I hate recursive dfs....Too manyy parameters!!!!
+    # a recursive way use permute itself with short lines
+    # this method is amazing! I didn't thought about this.
+    # Scarlett Chen
+    # 5/26/2015 tue 10:14 pm
+    # 46 Permutations
+    def permute(self, nums):
+        if len(nums)<=1:
+            return [nums]
+        perm=[]
+        for idx,ele in enumerate(nums):
+            for rest in self.permute(nums[:idx]+nums[idx+1:]):
+                if rest:
+                    perm.append([ele]+rest)
+        return perm
